293f090166bd1c65543bf8be90444d6085f09826
suuuuuu06 <bsj06@sookmyung.ac.kr>
2022-11-15 16:59:34 +0900

    05

--- main.c
+++ main.c
@@ -3,23 +3,23 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void){
-   int i;
-   int grade[5];
-   int sum=0;
-   
-   for(i=0;i<5;i++)
-   {
-      printf("grade[%i]= ",i);
-      scanf("%d",&grade[i]);
-   }
-   
-   for(i=0;i<5;i++)
-   {
-      sum+=*(grade+i);
-      printf("%i=%i\n",i,*(grade+i));
-      
-   }
-   printf("average: %i",sum/5);
-   return 0;
+int main(void)
+{
+	char*pc;
+	int*pi;
+	double*pd;
+	
+	pc=(char*)10000;
+	pi=(int*)10000;
+	pd=(double*)10000;
+	
+	printf("증가 전: pc=%d,pi=%d,pd=%d\n",pc,pi,pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후 : pc=%d,pi=%d,pd=%d\n",pc,pi,pd);
+	
+	return 0; 
+	
 }
